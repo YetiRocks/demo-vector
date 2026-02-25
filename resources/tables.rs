4db06050fd86a15f4f7f4da6c5bb299330a85897
deliciousmonster <jaxon@deliciousmonster.com>
2026-02-25 03:17:44 -0700

    demo permissions

---
+++ resources/tables.rs
@@ -0,0 +1,8 @@
+use yeti_core::prelude::*;
+
+/// Article: public read + create (vector search demo)
+/// Users can generate new articles to explore semantic search.
+resource!(TableExtender for Article {
+    get => allow_read(),
+    post => allow_create(),
+});
