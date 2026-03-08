7eaae66a4b639eb5bb046827aa7c7ac06c64eb1e
deliciousmonster <jaxon@deliciousmonster.com>
2026-03-07 21:29:37 -0700

    simplify

--- resources/tables.rs
+++
@@ -1,9 +0,0 @@
-use yeti_core::prelude::*;
-
-// Article: full public CRUD (vector search demo)
-// Users can create, read, and delete articles to explore semantic search.
-resource!(TableExtender for Article {
-    get => allow_read(),
-    post => allow_create(),
-    delete => allow_delete(),
-});
