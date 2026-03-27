aed1365a6dad0097b667e858b2e383ac96d092bf
deliciousmonster <jaxon@deliciousmonster.com>
2026-03-27 16:04:24 -0700

    auth

--- source/src/main.tsx
+++ source/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './yeti.css'
+import './auth.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
